af84ddf7f97a4b88ce6a0bb257e01b407b1486b5
Dezyuk <romanyuk.groz@gmail.com>
2023-10-28 15:16:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Lab7;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PartsWarehouse part1 = new PartsWarehouse("Drill", "fds51s1", 50.584, 150);
+        PartsWarehouse part2 = new PartsWarehouse("Pump", "5d8sf11c", 1000.00, 58);
+        PartsWarehouse part3 = new PartsWarehouse("Valve", "9df2q3f", 405.35, 7);
+        PartsWarehouse part4 = new PartsWarehouse("Sealer", "a1f4y2n", 18.99, 200);
+
+
+        Storehouse store = new Storehouse();
+        store.AddParts(part1);
+        store.AddParts(part2);
+        store.AddParts(part3);
+        store.AddParts(part4);
+
+        Console.WriteLine(store);
+        Console.WriteLine("--------------------------------------------------");
+        store.RemovePart(part1);
+
+        Console.WriteLine(store);
+    }
+}
---
+++ Storehouse.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Text;
+
+namespace Lab7
+{
+    /// <summary>
+    /// Storehouse — класс для управления запасными частями в складе.
+    /// </summary>
+    public class Storehouse
+    {
+
+        List<PartsWarehouse> warehouseList = new List<PartsWarehouse>();
+
+
+        /// <summary>
+        /// Добавление новой запчасти в List.
+        /// </summary>
+        /// <param name="newPart">Новая деталь которую необходимо добавить, проверяется есть ли уже данный объект в List, а так-же на схожесть Id.</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void AddParts(PartsWarehouse newPart)
+        {
+            if (newPart is not null && !warehouseList.Contains(newPart))
+            {
+                foreach (PartsWarehouse part in warehouseList)
+                {
+                    if (part.Id.Equals(newPart.Id))
+                    {
+                        throw new ArgumentOutOfRangeException("This part is already in Storage!");
+                    }
+                }
+                warehouseList.Add(newPart);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("This part is already in Storage!");
+            }
+        }
+
+        /// <summary>
+        /// Удаление запчасти из List.
+        /// </summary>
+        /// <param name="partToRemove">Деталь которую необходимо удалить.</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void RemovePart(PartsWarehouse partToRemove)
+        {
+            if (partToRemove is not null && warehouseList.Contains(partToRemove))
+            {
+                warehouseList.Remove(partToRemove);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("This part does not exist!");
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            foreach (PartsWarehouse part in warehouseList)
+            {
+                str.Append(part + "\n");
+            }
+            return str.ToString();
+        }
+    }
+}
